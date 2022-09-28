ba8228c54a81c9a78804c2135ea75a49a1fe4f3c
Busra Batu <busraanac4@gmail.com>
2022-09-28 11:26:46 +0300

    file layout changed

--- index.js
+++
@@ -1,3 +0,0 @@
-import getData from "./App.js";
-
-getData(1).then(data=>console.log(data));
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
+  {/*PRACTICE 1
+      
+      import axios from "axios";
+
+ const getData=async (userId)=>{
+  const {data: user}=await axios("https://jsonplaceholder.typicode.com/users/"+userId);
+  const{data: postData}=await axios("https://jsonplaceholder.typicode.com/posts?id="+ userId);
+
+
+  return {user, postData};
+}
+export default getData;*/}
